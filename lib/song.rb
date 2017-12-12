50cbec38cf61d362c4e467e18a7e7ecaaeb63085
James Ferguson <ncjamesferguson@gmail.com>
2017-12-12 09:40:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,12 @@ class Song
     self.all.sort_by{|song| song.name}
   end
 
-  def self.new_from_filename
+  def self.new_from_filename(name)
+    song = Song.new
+    arr = name.delete(".mp3").strip.split("-")
+    song.name = arr[1]
+    song.artist_name = arr[0]
+    song
   end
 
 end
